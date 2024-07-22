83dc4c3703bacf4e1359786e47556698e420cc3a
Behnam2064 <behnamgraphic3@gmail.com>
2024-07-22 11:15:11 -0700

    feat:The Share data has been added  Share data in windows with wpf chore: The Share data test has been added

---
+++ WinRTHelper/SharingDataWinTest/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WinRTHelper.SharingData;
+
+namespace SharingDataWinTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void OnShareText1(object sender, RoutedEventArgs e)
+        {
+            ShareIt it = new ShareIt(this, "My text", "Share my text");
+
+            it.Share(tb1.Text, ShareMode.Text);
+        }
+
+        private void OnShareImage1(object sender, RoutedEventArgs e)
+        {
+            ShareIt it = new ShareIt(this, "My image", "Share my image");
+
+            it.Share("image01.jpg", ShareMode.File);
+        }
+    }
+}
---
+++ WinRTHelper/WinRTHelper/SharingData/ShareIt.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace WinRTHelper.SharingData
+{
+    public enum ShareMode
+    {
+        Blank,
+        Text,
+        Weblink,
+        File,
+    }
+
+    /// <summary>
+    /// https://learn.microsoft.com/en-us/windows/uwp/app-to-app/share-data
+    /// https://learn.microsoft.com/en-us/uwp/api/windows.applicationmodel.datatransfer.datapackage?view=winrt-22621
+    /// </summary>
+    public class ShareIt
+    {
+
+        private IntPtr hwnd { get; set; }
+
+        private DataTransferManager dtm { get; set; }
+
+        public ShareMode Mode { get; private set; }
+
+        public object Content { get; private set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        private Window window;
+
+        public ShareIt(Window window, string Title, string Description)
+        {
+            this.window = window;
+            hwnd = new WindowInteropHelper(window).Handle;
+            //var s = DataTransferManager.GetForCurrentView();
+            dtm = DataTransferManagerHelper.GetForWindow(hwnd);
+            //dtm.ShareProvidersRequested += Dtm_ShareProvidersRequested;
+            //dtm.TargetApplicationChosen += Dtm_TargetApplicationChosen;
+            dtm.DataRequested += OnDataRequested;
+            this.Title = Title;
+            this.Description = Description;
+        }
+
+        private void Dtm_TargetApplicationChosen(DataTransferManager sender, TargetApplicationChosenEventArgs args)
+        {
+            Debug.WriteLine("TargetApplicationChosen");
+        }
+
+        private void Dtm_ShareProvidersRequested(DataTransferManager sender, ShareProvidersRequestedEventArgs args)
+        {
+            Debug.WriteLine("ShareProvidersRequested");
+        }
+
+        public ShareIt(Window window) : this(window, string.Empty, string.Empty) { }
+
+        async void OnDataRequested(DataTransferManager sender, DataRequestedEventArgs args)
+        {
+            var deferral = args.Request.GetDeferral();
+            args.Request.Data.Properties.Title = Title;
+            args.Request.Data.Properties.Description = Description;
+
+
+            DataPackage dp = args.Request.Data;
+            //dp.Properties.Title = DataPackageTitle.Text;
+            Debug.WriteLine("IsSupported: " + DataTransferManager.IsSupported());
+
+            switch (Mode)
+            {
+                case ShareMode.Text:
+                    dp.SetText(Content.ToString());
+                    break;
+
+                case ShareMode.Weblink:
+                    dp.SetWebLink(new System.Uri(Content.ToString()));
+                    break;
+
+                /*               case ShareMode.StorageItem:
+                                   var filesToShare = new List<IStorageItem>();
+                                   StorageFile imageFile = await StorageFile.GetFileFromPathAsync(AppDomain.CurrentDomain.BaseDirectory + Content.ToString());
+                                   filesToShare.Add(imageFile);
+
+                                   dp.SetStorageItems(filesToShare);
+                                   break;
+           */
+                case ShareMode.File:
+                    List<IStorageItem> filesToShare = new List<IStorageItem>();
+                    if (Content is string)
+                    {
+                        if (System.IO.File.Exists(Content.ToString()))
+                        {
+                            filesToShare.Add(await GetPathAsync(Content.ToString()));
+                        }
+                        else
+                            throw new System.IO.FileNotFoundException();
+                    }
+                    else if (Content is IEnumerable<string>)
+                    {
+                        var ls = Content as List<string>;
+                        if (ls != null && ls.Count > 0)
+                        {
+                            foreach (string item in ls)
+                                if (System.IO.File.Exists(item))
+                                {
+                                    filesToShare.Add(await GetPathAsync(item));
+                                }
+                                else
+                                    throw new System.IO.FileNotFoundException();
+                        }
+                    }
+                    //https://stackoverflow.com/questions/54570111/how-can-i-pass-an-implementation-of-istorageitem-to-datapackage-setstorageitem
+                    if (filesToShare.Count > 0)
+                        dp.SetStorageItems(filesToShare, false);
+
+                    break;
+            }
+
+            deferral.Complete();
+        }
+
+
+        private async Task<StorageFile> GetPathAsync(string path)
+        {
+            if (Path.IsPathRooted(Content.ToString()))
+            {
+                //Absolute Path => "C:\Temp\myfile.txt"
+                return await StorageFile.GetFileFromPathAsync(path);
+            }
+            else
+            {
+                //Relative Path => "myfile.txt" or "..\temp\mytext.txt"
+                return await StorageFile.GetFileFromPathAsync(Path.GetFullPath(path));
+            }
+        }
+
+        public void Share(object content, ShareMode shareMode)
+        {
+            Mode = shareMode;
+            Content = content;
+            IntPtr hwnd = new WindowInteropHelper(window).Handle;
+            //Debug.WriteLine("IntPtr window:" + hwnd.ToString());
+            DataTransferManagerHelper.ShowShareUIForWindow(hwnd);
+        }
+
+    }
+}

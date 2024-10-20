46061d28d2acd72b612f9eceefa4f7f26aae29d2
Behnam2064 <behnamgraphic3@gmail.com>
2024-10-20 17:31:18 +0330

    chore:The AssemblyInfo has been updatd to nuget

--- WinRTHelper/WinRTHelper/Properties/AssemblyInfo.cs
+++ WinRTHelper/WinRTHelper/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WinRTHelper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Classes for easier use of some Microsoft APIs")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WinRTHelper")]
